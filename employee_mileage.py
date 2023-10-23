c3156ad73c573b1a0d1009f11e705ad26b4125b8
imarcosm <imarcosm@ies-sabadell.cat>
2023-10-23 12:16:44 +0200

    employee_mileage.py mal hecho

--- employee_mileage.py
+++ employee_mileage.py
@@ -5,4 +5,24 @@ class EmployeeMileage(ModelSQL, ModelView):
     "Employee Mileage"
     __name__ = 'employee_mileage'
     
+    employee = fields.Many2One('company.employee', 'Employee', required=True)
+    resource = fields.Reference('Resource', selection='get_resource')
+    address = fields.Many2One('party.address', 'Address', required=True)
+    distance = fields.float('Distance', required=True)
+    date = fields.date('Date', required=True)
+    description = fields.Char('Description')
     
+    # Resource functions -> Puede resultar en catástrofe
+    @classmethod
+    def _get_resource(cls):
+        'Return list for resources' # Docstring?
+        return ['project.work', 'sale.opportunity'] # Modelos que podrán ser seleccionados?
+    
+    @classmethod
+    def get_resource(cls):
+        Model = Pool().get('employee.mileage')  # Posible error
+        models = cls._get_resource()
+        models = Model.search([
+            ('model', 'in', models),
+        ])
+        return [('', '')] + [(m.model, m.name) for m in models]
