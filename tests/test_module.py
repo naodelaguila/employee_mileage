e7e65708984bc68ff00570cae90a9e2a51afda8a
ndelaguila <ndelaguilaa@ies-sabadell.cat>
2023-11-03 10:05:25 +0100

    cosas para el test -naomy

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,7 +1,6 @@
 
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-
 from trytond.modules.company.tests import CompanyTestMixin
 from trytond.tests.test_tryton import ModuleTestCase
 
@@ -9,6 +8,5 @@ from trytond.tests.test_tryton import ModuleTestCase
 class QuizTestCase(CompanyTestMixin, ModuleTestCase):
     'Test Employee mileage module'
     module = 'employee_mileage'
-
 
 del ModuleTestCase
